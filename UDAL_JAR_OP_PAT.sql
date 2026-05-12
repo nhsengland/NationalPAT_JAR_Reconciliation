8a7cee777012c31a40e0e74f2b016450b2b8ae92
Destiny Bradley <destiny.bradley@nhs.net>
2026-05-12 12:23:21 +0100

    Update UDAL_JAR_OP_PAT.sql

--- UDAL_JAR_OP_PAT.sql
+++ UDAL_JAR_OP_PAT.sql
@@ -16,6 +16,7 @@ SELECT
 	 WHEN Provider_Current = 'R1G' THEN 'RA9'
 	 WHEN Provider_Current = 'RVJ13' THEN 'RVJ'
 	 WHEN Provider_Current = 'RA4' THEN 'RH5'
+	 WHEN Provider_Current = 'B4B4S' THEN 'RD1'
 	 ELSE Provider_Current END AS 'Provider_Current'
 -- For assurance, we add in a case statement on Provider names to make sure that all mergers and name changes are correctly captured
 
@@ -26,7 +27,8 @@ SELECT
 	 WHEN o.Organisation_name  = 'POOLE HOSPITAL NHS FOUNDATION TRUST' THEN 'UNIVERSITY HOSPITAL DORSET NHS FOUNDATION TRUST'
 	 WHEN o.Organisation_name  = 'TORBAY AND SOUTHERN DEVON HEALTH AND CARE NHS TRUST' THEN 'TORBAY AND SOUTH DEVON NHS FOUNDATION TRUST'
 	 When o.Organisation_name = 'EMERSONS GREEN NHS TREATMENT CENTRE' THEN 'NORTH BRISTOL NHS TRUST'
-	  When o.Organisation_name = 'YEOVIL DISTRICT HOSPITAL NHS FOUNDATION TRUST' THEN 'SOMERSET NHS FOUNDATION TRUST' ELSE o.Organisation_name  END AS 'orgname'
+	 WHEN o.organisation_name = 'BSW BANES LOCALITY CDC' THEN 'ROYAL UNITED HOSPITALS BATH NHS FOUNDATION TRUST'
+	 When o.Organisation_name = 'YEOVIL DISTRICT HOSPITAL NHS FOUNDATION TRUST' THEN 'SOMERSET NHS FOUNDATION TRUST' ELSE o.Organisation_name  END AS 'orgname'
 
 -- System names for the Provider
 
@@ -34,7 +36,7 @@ SELECT
 
 -- Isolate the Treatment Function Code and name the column appropriately
 
-	,left(op.Dimention_3,3) as 'TFC'
+	,Left(op.Dimention_3,3) as 'TFC'
 
 -- Isolate the HRG and name the column appropriately
 
@@ -56,6 +58,7 @@ SELECT
 
 	,CASE WHEN [Dimention_1] LIKE 'Follow%' THEN 'Outpatient Follow-Up' WHEN [Dimention_1] LIKE '1st%' THEN 'Outpatient First Appointment' END as [High_Level_Pod]	
 
+	,[Dimention_4]
 -- Sum all unadjusted activity - As submitted	
 
 	,SUM(op.[unadjusted]) as 'Total_Activity(Unadj)'
@@ -65,13 +68,13 @@ SELECT
 	,SUM(op.[adjusted]) as 'Total_Activity(Adj)'
 
 
-FROM [UDALSQLMART_PatActivity].[PAT_Intermediate_Table_OP] op
+FROM [UDALLAKEMART_PatActivity].[PAT_Intermediate_Table_OP] op
 
 
 --Join to TFC table to bring in description 
 
 left join [Internal_Reference].[UCSM_RefTFCs] as tfc
-on Right(op.Dimention_3,3) = tfc.[Treatment_Function_Code]
+on Left(op.Dimention_3,3) = tfc.[Treatment_Function_Code]
 
 --Join to reference tables to bring in provider and system names
 
@@ -80,7 +83,7 @@ ON op.Provider_Current = o.Organisation_Code COLLATE Latin1_General_CI_AS
 
 
 WHERE 			
-	op.Attendance_Date >= '2024-03-01' -- current FY, YTD
+	op.Attendance_Date >= '2025-04-01' -- current FY, YTD
 
 -- add in the below if you are looking at a range (for e.g between financial years)
  and op.[Attendance_Date] < '2026-04-01'
@@ -89,8 +92,8 @@ WHERE
 		, 'Devolved Administration', 'Devolved Administration*', 'Reciprocal OSVs', 'Non-reciprocal OSVs', 'OSV (Type Unknown)', 'OSV (Type Unknown)*') -- CAM Logic alighned with national pat
  	and [Dimention_1] <> 'Unknown Appointment Type' -- exclude unknown appointments
 	and op.[Commissioner_Type] <> 'Private Patient' -- Excluding private patients
-	and [Dimention_4] = 'Consultant led: Specific Acute' -- National planning guidance is specific acute only - excluding some maternity and LDA MH activity
---	and left(op.Dimention_3,3) not in ('812', '360')
+	--and [Dimention_4] = 'Consultant led: Specific Acute' -- National planning guidance is specific acute only - excluding some maternity and LDA MH activity
+	and Left(op.Dimention_3,3) not in ('812', '360')
 	and op.[Provider_Current] in ( 
 	'RD1',
 	'RN3',
@@ -104,20 +107,43 @@ WHERE
 	'RBD',
 	'R0D',
 	'RTE',
-	'RH5')
+    'RH5',
+	'RVJ13', 
+	'B4B4S')
 
 
 GROUP BY	
 
-	 Provider_Current
-	 ,o.Organisation_name
+	CASE WHEN Provider_Current = 'RD3' THEN 'R0D'
+	 WHEN Provider_Current = 'RDZ' THEN 'R0D'
+	 WHEN Provider_Current = 'RBZ' THEN 'RH8'
+	 WHEN Provider_Current = 'RA3' THEN 'RA7'
+	 WHEN Provider_Current = 'RBA' THEN 'RH5'
+	 WHEN Provider_Current = 'R1G' THEN 'RA9'
+	 WHEN Provider_Current = 'RVJ13' THEN 'RVJ'
+	 WHEN Provider_Current = 'RA4' THEN 'RH5'
+	 WHEN Provider_Current = 'B4B4S' THEN 'RD1'
+	 ELSE Provider_Current END
+-- For assurance, we add in a case statement on Provider names to make sure that all mergers and name changes are correctly captured
+
+  ,CASE WHEN o.Organisation_name = 'ROYAL DEVON AND EXETER NHS FOUNDATION TRUST' THEN 'ROYAL DEVON UNIVERSITY HEALTHCARE NHS FOUNDATION TRUST'
+	 WHEN o.Organisation_name  = 'TAUNTON AND SOMERSET NHS FOUNDATION TRUST' THEN 'SOMERSET NHS FOUNDATION TRUST'
+	 WHEN o.Organisation_name  = 'NORTHERN DEVON HEALTHCARE NHS TRUST' THEN 'ROYAL DEVON UNIVERSITY HEALTHCARE NHS FOUNDATION TRUST'
+	 WHEN o.Organisation_name  = 'THE ROYAL BOURNEMOUTH AND CHRISTCHURCH HOSPITALS NHS FOUNDATION TRUST' THEN 'UNIVERSITY HOSPITAL DORSET NHS FOUNDATION TRUST'
+	 WHEN o.Organisation_name  = 'POOLE HOSPITAL NHS FOUNDATION TRUST' THEN 'UNIVERSITY HOSPITAL DORSET NHS FOUNDATION TRUST'
+	 WHEN o.Organisation_name  = 'TORBAY AND SOUTHERN DEVON HEALTH AND CARE NHS TRUST' THEN 'TORBAY AND SOUTH DEVON NHS FOUNDATION TRUST'
+	 When o.Organisation_name = 'EMERSONS GREEN NHS TREATMENT CENTRE' THEN 'NORTH BRISTOL NHS TRUST'
+	 WHEN o.organisation_name = 'BSW BANES LOCALITY CDC' THEN 'ROYAL UNITED HOSPITALS BATH NHS FOUNDATION TRUST'
+	 When o.Organisation_name = 'YEOVIL DISTRICT HOSPITAL NHS FOUNDATION TRUST' THEN 'SOMERSET NHS FOUNDATION TRUST' ELSE o.Organisation_name  END
 	 ,o.STP_Name
-	,left(op.Dimention_3,3)
+	,Left(op.Dimention_3,3)
 	,Dimention_7
 	,PAT_Commissioner_Type
 	,tfc.Treatment_Function_Desc
-	,Attendance_Date
-	,Dimention_1
+	,CONCAT(YEAR(Attendance_Date),FORMAT(MONTH(Attendance_Date),'00'))
+	,CASE WHEN [Dimention_1] LIKE 'Follow%' THEN 'Outpatient Follow-Up' WHEN [Dimention_1] LIKE '1st%' THEN 'Outpatient First Appointment' END
+	,Dimention_1	
+	,[Dimention_4]
 
 
 GO
